dd760b793ea193a584b1b5a9a3fb7ab71344f22b
Nicolau Romanos <nfromanos@hotmail.com>
2022-07-04 09:34:16 -0300

    Teste

---
+++ src/main/java/services/OscarServices.java
@@ -0,0 +1,14 @@
+package services;
+
+import models.OscarModel;
+
+import java.util.List;
+
+public class OscarServices {
+
+    private final List<OscarModel> oscarModel;
+
+    public OscarServices(List<OscarModel> oscarModel) {
+        this.oscarModel = oscarModel;
+    }
+}
